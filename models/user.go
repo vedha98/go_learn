67fb6a6790e1cecca658aed45bf9817fe27b28e5
vedha98 <vetha.gnanam98@gmail.com>
2020-05-13 17:47:18 +0530

    added bcrypt

--- models/user.go
+++ models/user.go
@@ -1,9 +1,24 @@
 package models
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	ID       uint   `json:"id" gorm:"primary_key"`
 	Name     string `json:"name"`
 	Password string `json:"password"`
 	Email    string `json:"email"`
 	Books    []Book
+}
+
+func Hash(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func (u *User) BeforeSave() error {
+	hashedPassword, err := Hash(u.Password)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
 }
