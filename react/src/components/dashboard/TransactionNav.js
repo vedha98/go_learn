6551eb489e5b7b490011726087b6ba4c139cc67f
vedha98 <vetha.gnanam98@gmail.com>
2020-05-19 15:11:58 +0530

    css and icons fix

--- react/src/components/dashboard/TransactionNav.js
+++ react/src/components/dashboard/TransactionNav.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 export class TransactionNav extends Component {
     constructor(props) {
         super(props);
@@ -9,6 +10,7 @@ export class TransactionNav extends Component {
     render() {
         return (
             <div className="transnav-wrap">
+                <div style={{display:"flex"}}>
                <div onClick={e=>this.props.setstep(0)} className={this.props.stage===0?"transnav-item active":"transnav-item"}>
                 Send Money
                </div>
@@ -18,15 +20,9 @@ export class TransactionNav extends Component {
                <div onClick={e=>this.props.setstep(2)} className={this.props.stage===2?"transnav-item active":"transnav-item"}>
                 Passbook
                </div>
+               </div>
                <div onClick={this.props.closewindow} className="transnav-item close">
-               {/* <svg className="cross">
-               <path  d="M98.159,150.539L10.858,237.84c-14.461,14.469-14.461,37.936,0,52.397
-		c14.453,14.453,37.92,14.453,52.372,0l87.309-87.317l87.293,87.317c14.469,14.453,37.92,14.453,52.381,0
-		c14.477-14.469,14.461-37.936,0-52.397l-87.293-87.301l87.301-87.285c14.469-14.477,14.469-37.936,0-52.397
-		c-14.469-14.477-37.92-14.477-52.389,0l-87.293,87.309L63.23,10.858c-14.453-14.477-37.92-14.477-52.381,0
-		c-14.461,14.461-14.461,37.895,0,52.364L98.159,150.539z"/>
-               </svg> */}
-               close
+               <FontAwesomeIcon icon={faTimes} />
                </div>
             </div>
         );
--- react/src/components/dashboard/TransactionSearch.js
+++ react/src/components/dashboard/TransactionSearch.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {filtertransactions} from '../../actions/transactionActions';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 export class TransactionSearch extends Component {
     constructor(props) {
         super(props);
@@ -16,7 +18,9 @@ export class TransactionSearch extends Component {
     render() {
         return (
             <div className="search-wrap">
+                <FontAwesomeIcon icon={faSearch} color="white" className="search-icon"/>
                 <input className="search-input" val={this.state.val} onChange={this.handleChange} placeholder="Search transactions" className="transaction-search"></input>
+                
             </div>
         );
     }
